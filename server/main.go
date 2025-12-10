cb57334bcdf958a1c4c666f0d26c3b074f14adb3
Adrian <adrian@computers.si>
2025-12-11 00:00:23 +0100

    50% done

--- server/main.go
+++ server/main.go
@@ -2,34 +2,33 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"log"
-	"net"
-	rpb "server/razpravljalnica"
-	"sync"
-	"sync/atomic"
-	"time"
-
 	"github.com/alecthomas/kong"
 	"google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"log"
+	"net"
+	rpb "server/razpravljalnica"
+	"sync"
 )
 
 type RunNodeCmd struct {
 	ControllAddress string `arg:""`
 }
 
+// TODO forward state
 func (s *RunNodeCmd) Run() error {
+	var msrv = MessageBoardServer{}
+
 	lis, err := net.Listen("tcp", ":50051") // TODO rm this port
 	if err != nil {
 		return fmt.Errorf("failed to listen: %v", err)
 	}
 
 	srv := grpc.NewServer()
-	var msrv = MessageBoardServer{}
 	rpb.RegisterMessageBoardServer(srv, &msrv)
 
 	log.Printf("server listening at %v", lis.Addr())
@@ -55,58 +54,161 @@ type MessageBoardServer struct {
 
 type Topic struct {
 	Name        string
-	Messages    []Message
+	Message_idx int64
+	Messages    map[int64]Message
 	Subscribers []struct {
-		Token string
-		ch    chan<- MessageEv
+		Token         string
+		Target_server rpb.NodeInfo
+		ch            chan<- MessageEv
 	}
+}
+
+type Message struct {
+	UserId    int64
+	Text      string
+	CreatedAt *timestamppb.Timestamp
+	Likes     map[int64]struct{}
 }
 
 type User struct {
 	Name string
 }
-type Message struct {
-	User       int64
-	Text       string
-	Created_at struct{}
-	Likes      atomic.Int32
+
+type MessageEv struct {
+	SequenceNumber int64
+	Op             rpb.OpType
+	Message        *Message
+	EventAt        *timestamppb.Timestamp
 }
-type MessageEv struct{}
 
 func (s *MessageBoardServer) CreateTopic(_ context.Context, req *rpb.CreateTopicRequest) (*rpb.Topic, error) {
-	name := req.GetName()
-	if name == "" {
-		return nil, status.Error(codes.InvalidArgument, "Missing Topic name")
-	}
 	s.topics_mtx.Lock()
 	defer s.topics_mtx.Unlock()
 
-	s.Topics = append(s.Topics, Topic{Name: name})
+	name := req.Name
+	if len(name) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Missing Topic name")
+	}
+
+	s.Topics = append(s.Topics, Topic{Name: name, Messages: map[int64]Message{}})
 	return &rpb.Topic{Id: int64(len(s.Topics) - 1), Name: name}, nil
 }
 func (s *MessageBoardServer) CreateUser(_ context.Context, req *rpb.CreateUserRequest) (*rpb.User, error) {
-	name := req.GetName()
-	if name == "" {
-		return nil, status.Error(codes.InvalidArgument, "Missing User name")
-	}
 	s.users_mtx.Lock()
 	defer s.users_mtx.Unlock()
+
+	name := req.Name
+	if len(name) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Missing User name")
+	}
 
 	s.Users = append(s.Users, User{Name: name})
 	return &rpb.User{Id: int64(len(s.Users) - 1), Name: name}, nil
 }
-func (s *MessageBoardServer) DeleteMessage(_ context.Context, _ *rpb.DeleteMessageRequest) (*emptypb.Empty, error) {
-	return nil, status.Error(codes.Unimplemented, "method DeleteMessage not implemented")
+func (s *MessageBoardServer) DeleteMessage(_ context.Context, req *rpb.DeleteMessageRequest) (*emptypb.Empty, error) {
+	// TODO subs
+	s.topics_mtx.Lock()
+	defer s.topics_mtx.Unlock()
+
+	var topic_id int64
+	if t, err := s.topicExist(req.TopicId); err != nil {
+		return nil, err
+	} else {
+		topic_id = t
+	}
+	var user_id int64
+	if u, err := s.userExist(req.UserId); err != nil {
+		return nil, err
+	} else {
+		user_id = u
+	}
+	topic := &s.Topics[topic_id]
+	var message_id int64
+	if u, err := s.msgExist(topic_id, req.MessageId); err != nil {
+		return nil, err
+	} else {
+		message_id = u
+	}
+	if topic.Messages[message_id].UserId != user_id {
+		return nil, status.Error(codes.PermissionDenied, "Can't delete other user's message")
+	}
+	delete(topic.Messages, message_id)
+	return &emptypb.Empty{}, nil
 }
-func (s *MessageBoardServer) GetMessages(_ context.Context, _ *rpb.GetMessagesRequest) (*rpb.GetMessagesResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "method GetMessages not implemented")
+func (s *MessageBoardServer) GetMessages(_ context.Context, req *rpb.GetMessagesRequest) (*rpb.GetMessagesResponse, error) {
+	s.topics_mtx.RLock()
+	defer s.topics_mtx.RUnlock()
+
+	out := make([]*rpb.Message, 0, req.Limit)
+	var topic_id int64
+	if t, err := s.topicExist(req.TopicId); err != nil {
+		return nil, err
+	} else {
+		topic_id = t
+	}
+	topic := &s.Topics[topic_id]
+	limit := req.Limit
+	for k, v := range topic.Messages {
+		if limit <= 0 {
+			break
+		}
+		if k >= req.FromMessageId {
+			out = append(out, &rpb.Message{
+				Id:        k,
+				TopicId:   topic_id,
+				UserId:    v.UserId,
+				Text:      v.Text,
+				CreatedAt: v.CreatedAt,
+				Likes:     int32(len(v.Likes)),
+			})
+			limit--
+		}
+	}
+	return &rpb.GetMessagesResponse{Messages: out}, nil
 }
+
 func (s *MessageBoardServer) GetSubscriptionNode(_ context.Context, _ *rpb.SubscriptionNodeRequest) (*rpb.SubscriptionNodeResponse, error) {
 	return nil, status.Error(codes.Unimplemented, "method GetSubcscriptionNode not implemented")
 }
-func (s *MessageBoardServer) LikeMessage(_ context.Context, _ *rpb.LikeMessageRequest) (*rpb.Message, error) {
-	return nil, status.Error(codes.Unimplemented, "method LikeMessage not implemented")
+
+func (s *MessageBoardServer) LikeMessage(_ context.Context, req *rpb.LikeMessageRequest) (*rpb.Message, error) {
+	// TODO subs
+	s.topics_mtx.Lock()
+	defer s.topics_mtx.Unlock()
+	var topic_id int64
+	if t, err := s.topicExist(req.TopicId); err != nil {
+		return nil, err
+	} else {
+		topic_id = t
+	}
+	topic := &s.Topics[topic_id]
+	var user_id int64
+	if u, err := s.userExist(req.UserId); err != nil {
+		return nil, err
+	} else {
+		user_id = u
+	}
+	var message_id int64
+	if u, err := s.msgExist(topic_id, req.MessageId); err != nil {
+		return nil, err
+	} else {
+		message_id = u
+	}
+	message := topic.Messages[message_id]
+	if _, ok := message.Likes[user_id]; ok {
+		return nil, status.Error(codes.AlreadyExists, "User already liked message")
+	}
+	message.Likes[user_id] = struct{}{}
+	return &rpb.Message{
+		Id:        message_id,
+		TopicId:   topic_id,
+		UserId:    user_id,
+		Text:      message.Text,
+		CreatedAt: message.CreatedAt,
+		Likes:     int32(len(message.Likes)),
+	}, nil
 }
+
 func (s *MessageBoardServer) ListTopics(_ context.Context, _ *emptypb.Empty) (*rpb.ListTopicsResponse, error) {
 	s.topics_mtx.RLock()
 	defer s.topics_mtx.RUnlock()
@@ -116,17 +218,100 @@ func (s *MessageBoardServer) ListTopics(_ context.Context, _ *emptypb.Empty) (*r
 	}
 	return &rpb.ListTopicsResponse{Topics: out}, nil
 }
-func (s *MessageBoardServer) PostMessage(_ context.Context, _ *rpb.PostMessageRequest) (*rpb.Message, error) {
-	return nil, status.Error(codes.Unimplemented, "method PostMessage not implemented")
-}
-func (s *MessageBoardServer) SubscribeTopic(req *rpb.SubscribeTopicRequest, _ grpc.ServerStreamingServer[rpb.MessageEvent]) error {
+
+func (s *MessageBoardServer) PostMessage(_ context.Context, req *rpb.PostMessageRequest) (*rpb.Message, error) {
+	// TODO subs
 	s.topics_mtx.Lock()
 	defer s.topics_mtx.Unlock()
 
+	var topic_id int64
+	if t, err := s.topicExist(req.TopicId); err != nil {
+		return nil, err
+	} else {
+		topic_id = t
+	}
+	var user_id int64
+	if u, err := s.userExist(req.UserId); err != nil {
+		return nil, err
+	} else {
+		user_id = u
+	}
+	if len(req.Text) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Empty message not permited")
+	}
+
+	topic := &s.Topics[topic_id]
+	id := topic.Message_idx
+	topic.Message_idx++
+	msg := Message{UserId: user_id, Text: req.Text, CreatedAt: timestamppb.Now(), Likes: map[int64]struct{}{}}
+	topic.Messages[id] = msg
+	return &rpb.Message{Id: id, UserId: msg.UserId, Text: msg.Text, CreatedAt: msg.CreatedAt, Likes: int32(len(msg.Likes))}, nil
+}
+
+func (s *MessageBoardServer) SubscribeTopic(req *rpb.SubscribeTopicRequest, _ grpc.ServerStreamingServer[rpb.MessageEvent]) error {
 	return status.Error(codes.Unimplemented, "method SubscribeTopic not implemented")
 }
-func (s *MessageBoardServer) UpdateMessage(_ context.Context, _ *rpb.UpdateMessageRequest) (*rpb.Message, error) {
-	return nil, status.Error(codes.Unimplemented, "method UpdateMessage not implemented")
+
+func (s *MessageBoardServer) UpdateMessage(_ context.Context, req *rpb.UpdateMessageRequest) (*rpb.Message, error) {
+	// TODO subs
+	s.topics_mtx.Lock()
+	defer s.topics_mtx.Unlock()
+	if len(req.Text) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Empty message not permited")
+	}
+	var topic_id int64
+	if t, err := s.topicExist(req.TopicId); err != nil {
+		return nil, err
+	} else {
+		topic_id = t
+	}
+	topic := &s.Topics[topic_id]
+	var user_id int64
+	if u, err := s.userExist(req.UserId); err != nil {
+		return nil, err
+	} else {
+		user_id = u
+	}
+	var message_id int64
+	if u, err := s.msgExist(topic_id, req.MessageId); err != nil {
+		return nil, err
+	} else {
+		message_id = u
+	}
+	message := topic.Messages[message_id]
+	message.Text = req.Text
+	topic.Messages[message_id] = message
+	return &rpb.Message{
+		Id:        message_id,
+		TopicId:   topic_id,
+		UserId:    user_id,
+		Text:      message.Text,
+		CreatedAt: message.CreatedAt,
+		Likes:     int32(len(message.Likes)),
+	}, nil
+}
+
+func (s *MessageBoardServer) topicExist(id int64) (int64, error) {
+	// expects lock
+	if int64(len(s.Topics)) > id {
+		return id, nil
+	}
+	return 0, fmt.Errorf("Topic %d doesn't exist", id)
+}
+
+func (s *MessageBoardServer) userExist(id int64) (int64, error) {
+	// expects lock
+	if int64(len(s.Users)) > id {
+		return id, nil
+	}
+	return 0, fmt.Errorf("User %d doesn't exist", id)
+}
+func (s *MessageBoardServer) msgExist(topic_id, id int64) (int64, error) {
+	// expects lock
+	if _, ok := s.Topics[topic_id].Messages[id]; ok {
+		return id, nil
+	}
+	return 0, fmt.Errorf("Message %d doesn't exist", id)
 }
 
 func main() {

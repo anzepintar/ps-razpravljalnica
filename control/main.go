a68332b0c711b40a37330bcfb4c846130679121c
Adrian Šiška <adrian@computers.si>
2026-01-05 14:32:50 +0100

    control plane first steps

--- control/main.go
+++ control/main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"github.com/alecthomas/kong"
+	"google.golang.org/grpc"
+	// codes "google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	// status "google.golang.org/grpc/status"
+	// emptypb "google.golang.org/protobuf/types/known/emptypb"
+	// "google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func main() {
+var cli RunControlCmd
 
+func main() {
+	ctx := kong.Parse(&cli)
+	err := ctx.Run()
+	ctx.FatalIfErrorf(err)
+}
+
+var grpc_security_opt = grpc.WithTransportCredentials(insecure.NewCredentials())
+
+type RunControlCmd struct {
+	BindAddr     string   `short:"b" help:"binds to this address"`
+	OtherMembers []string `arg:""`
+}
+
+func (s *RunControlCmd) Run() error {
+	fmt.Println(s)
+	return nil
 }

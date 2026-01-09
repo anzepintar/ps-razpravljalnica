344c78b67f5e86936c05d54c15620547d153c3d1
Adrian <adrian@computers.si>
2026-01-09 03:33:21 +0100

    add raft

--- control/go.mod
+++ control/go.mod
@@ -9,6 +9,26 @@ require (
 )
 
 require (
+	github.com/Jille/raft-grpc-leader-rpc v1.1.0 // indirect
+	github.com/Jille/raft-grpc-transport v1.6.1 // indirect
+	github.com/Jille/raftadmin v1.2.1 // indirect
+	github.com/armon/go-metrics v0.4.1 // indirect
+	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-hclog v1.6.2 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-metrics v0.5.4 // indirect
+	github.com/hashicorp/go-msgpack v0.5.5 // indirect
+	github.com/hashicorp/go-msgpack/v2 v2.1.2 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/golang-lru v1.0.2 // indirect
+	github.com/hashicorp/raft v1.7.3 // indirect
+	github.com/hashicorp/raft-boltdb v0.0.0-20251103221153-05f9dd7a5148 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
--- control/main.go
+++ control/main.go
@@ -1,23 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"slices"
 	"sync"
 	"time"
 
+	"context"
+	rpb "control/razpravljalnica"
+	"github.com/Jille/raft-grpc-leader-rpc/leaderhealth"
+	"github.com/Jille/raft-grpc-leader-rpc/rafterrors"
+	transport "github.com/Jille/raft-grpc-transport"
+	"github.com/Jille/raftadmin"
 	"github.com/alecthomas/kong"
+	"github.com/hashicorp/raft"
+	boltdb "github.com/hashicorp/raft-boltdb"
 	"google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
 	status "google.golang.org/grpc/status"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
-
-	// "google.golang.org/protobuf/types/known/timestamppb"
-	"context"
-	rpb "control/razpravljalnica"
+	"io"
 	"log"
 	"net"
+	"os"
+	"path/filepath"
+	"strconv"
 )
 
 var cli RunControlCmd
@@ -31,8 +40,10 @@ func main() {
 var grpc_security_opt = grpc.WithTransportCredentials(insecure.NewCredentials())
 
 type RunControlCmd struct {
-	BindAddr     string   `arg:"" help:"binds to this address"`
-	OtherMembers []string `arg:"" optional:"" help:"other control server addresses"`
+	BindAddr      string   `arg:"" help:"binds to this address"`
+	RaftID        string   `arg:"" help:"ID used by raft protocol"`
+	OtherMembers  []string `arg:"" optional:"" help:"other control server addresses"`
+	BootstrapRaft bool     `help:"Whether to bootstrap the Raft cluster"`
 }
 
 var cntsrv = ControlPlaneServer{}
@@ -58,14 +69,75 @@ func (s *RunControlCmd) Run() error {
 	if err != nil {
 		return fmt.Errorf("failed to listen: %v", err)
 	}
-	cntsrv.self = lis.Addr().String()
+
+	ctx := context.Background()
+	r, tm, err := NewRaft(ctx, s.RaftID, lis.Addr().String(), &cntsrv)
+	if err != nil {
+		return fmt.Errorf("failed to raft: %f", err)
+	}
+	cntsrv.raft = r
+
 	srv := grpc.NewServer(grpc.ChainUnaryInterceptor(myLog))
 	rpb.RegisterControlPlaneServer(srv, &cntsrv)
+	raftadmin.Register(srv, r)
+	leaderhealth.Setup(r, srv, []string{""})
+	tm.Register(srv)
 	log.Printf("server listening at %v", lis.Addr())
 	if err := srv.Serve(lis); err != nil {
 		return fmt.Errorf("failed to serve: %v", err)
 	}
 	return nil
+}
+
+func NewRaft(ctx context.Context, myID, myAddress string, fsm raft.FSM) (*raft.Raft, *transport.Manager, error) {
+	c := raft.DefaultConfig()
+	c.LocalID = raft.ServerID(myID)
+
+	raftDir := "runtime"
+	err := os.Mkdir(raftDir, 0750)
+
+	baseDir := filepath.Join(raftDir, myID)
+	err = os.Mkdir(baseDir, 0750)
+
+	ldb, err := boltdb.NewBoltStore(filepath.Join(baseDir, "logs.dat"))
+	if err != nil {
+		return nil, nil, fmt.Errorf(`boltdb.NewBoltStore(%q): %v`, filepath.Join(baseDir, "logs.dat"), err)
+	}
+
+	sdb, err := boltdb.NewBoltStore(filepath.Join(baseDir, "stable.dat"))
+	if err != nil {
+		return nil, nil, fmt.Errorf(`boltdb.NewBoltStore(%q): %v`, filepath.Join(baseDir, "stable.dat"), err)
+	}
+
+	fss, err := raft.NewFileSnapshotStore(baseDir, 3, os.Stderr)
+	if err != nil {
+		return nil, nil, fmt.Errorf(`raft.NewFileSnapshotStore(%q, ...): %v`, baseDir, err)
+	}
+
+	tm := transport.New(raft.ServerAddress(myAddress), []grpc.DialOption{grpc_security_opt})
+
+	r, err := raft.NewRaft(c, fsm, ldb, sdb, fss, tm.Transport())
+	if err != nil {
+		return nil, nil, fmt.Errorf("raft.NewRaft: %v", err)
+	}
+
+	if cli.BootstrapRaft {
+		cfg := raft.Configuration{
+			Servers: []raft.Server{
+				{
+					Suffrage: raft.Voter,
+					ID:       raft.ServerID(myID),
+					Address:  raft.ServerAddress(myAddress),
+				},
+			},
+		}
+		f := r.BootstrapCluster(cfg)
+		if err := f.Error(); err != nil {
+			return nil, nil, fmt.Errorf("raft.Raft.BootstrapCluster: %v", err)
+		}
+	}
+
+	return r, tm, nil
 }
 
 type Node struct {
@@ -80,7 +152,7 @@ type ControlPlaneServer struct {
 	Idx   int64
 	Nodes []Node
 
-	self string // TODO
+	raft *raft.Raft
 }
 
 func (s *ControlPlaneServer) GetClusterStateServer(ctx context.Context, req *rpb.GetClusterStateRequest) (*rpb.GetClusterStateResponse, error) {
@@ -94,6 +166,14 @@ func (s *ControlPlaneServer) GetClusterStateServer(ctx context.Context, req *rpb
 	var head *rpb.NodeInfo
 	var tail *rpb.NodeInfo
 
+	cf := s.raft.GetConfiguration()
+	conf := cf.Configuration()
+
+	out := make([]*rpb.ControlInfo, len(conf.Servers))
+	for i, v := range conf.Servers {
+		out[i] = &rpb.ControlInfo{Address: string(v.Address)}
+	}
+
 	if idx > 0 {
 		h := s.Nodes[idx-1]
 		head = &rpb.NodeInfo{NodeId: h.id, Address: h.address}
@@ -105,7 +185,7 @@ func (s *ControlPlaneServer) GetClusterStateServer(ctx context.Context, req *rpb
 	return &rpb.GetClusterStateResponse{
 		Head:    head,
 		Tail:    tail,
-		Servers: []*rpb.ControlInfo{{Address: s.self}}, // TODO
+		Servers: out, // TODO
 	}, nil
 }
 func (s *ControlPlaneServer) GetClusterStateClient(context.Context, *emptypb.Empty) (*rpb.GetClusterStateResponse, error) {
@@ -117,28 +197,31 @@ func (s *ControlPlaneServer) GetClusterStateClient(context.Context, *emptypb.Emp
 	}
 	head := s.Nodes[0]
 	tail := s.Nodes[len(s.Nodes)-1]
+	cf := s.raft.GetConfiguration()
+	conf := cf.Configuration()
+
+	out := make([]*rpb.ControlInfo, len(conf.Servers))
+	for i, v := range conf.Servers {
+		out[i] = &rpb.ControlInfo{Address: string(v.Address)}
+	}
+
 	return &rpb.GetClusterStateResponse{
 		Head:    &rpb.NodeInfo{NodeId: head.id, Address: head.address},
 		Tail:    &rpb.NodeInfo{NodeId: tail.id, Address: tail.address},
-		Servers: []*rpb.ControlInfo{{Address: s.self}}, // TODO
+		Servers: out, // TODO
 	}, nil
 }
 func (s *ControlPlaneServer) Register(ctx context.Context, req *rpb.RegisterRequest) (*rpb.NodeInfo, error) {
-	s.mtx.Lock()
-	defer s.mtx.Unlock()
-
-	if idx := slices.IndexFunc(s.Nodes, func(e Node) bool { return e.address == req.Address }); idx != -1 {
-		go s.removeNode(s.Nodes[idx].id, false)
+	f := s.raft.Apply(slices.Concat([]byte("+"), []byte(req.Address)), time.Second)
+	if err := f.Error(); err != nil {
+		return nil, rafterrors.MarkRetriable(err)
+	}
+	node, ok := f.Response().(*Node)
+	if !ok {
+		return nil, rafterrors.MarkRetriable(fmt.Errorf("Failed, to get a node commited"))
 	}
 
-	id := s.Idx
-	s.Idx++
-	s.Nodes = append(s.Nodes, Node{id: id, address: req.Address})
-	if len(s.Nodes) > 1 {
-		go s.sendUpdate(s.Nodes[len(s.Nodes)-2].id)
-	}
-
-	return &rpb.NodeInfo{NodeId: id, Address: req.Address}, nil
+	return &rpb.NodeInfo{NodeId: node.id, Address: node.address}, nil
 }
 func (s *ControlPlaneServer) Snitch(ctx context.Context, req *rpb.SnitchRequest) (*emptypb.Empty, error) {
 	go s.removeNode(req.NodeId, true)
@@ -176,25 +259,31 @@ func (s *ControlPlaneServer) sendUpdate(id int64) {
 	}
 }
 
-func (s *ControlPlaneServer) removeNode(id int64, shutdown bool) {
-	s.mtx.Lock()
-	defer s.mtx.Unlock()
-
+func (s *ControlPlaneServer) idToNode(id int64) *Node {
+	s.mtx.RLock()
+	defer s.mtx.RUnlock()
 	idx := slices.IndexFunc(s.Nodes, func(e Node) bool { return e.id == id })
 	if idx == -1 {
+		return nil
+	}
+	node := s.Nodes[idx]
+	return &node
+}
+
+func (s *ControlPlaneServer) removeNode(id int64, shutdown bool) {
+	if shutdown {
+		go sendShutdown(s.idToNode(id).address)
+	}
+	req := []byte("-")
+	req = strconv.AppendInt(req, id, 10)
+	f := s.raft.Apply(req, time.Second)
+	if err := f.Error(); err != nil {
+		if status.Code(err) == codes.Unavailable {
+			go s.removeNode(id, shutdown)
+		}
 		return
 	}
-	if idx > 0 {
-		go s.sendUpdate(s.Nodes[idx-1].id)
-	}
-	if idx < len(s.Nodes)-1 {
-		go s.sendUpdate(s.Nodes[idx+1].id)
-	}
-
-	if shutdown {
-		go sendShutdown(s.Nodes[idx].address)
-	}
-	s.Nodes = slices.Delete(s.Nodes, idx, idx+1)
+	_ = f.Response()
 }
 
 func sendShutdown(addr string) {
@@ -208,4 +297,91 @@ func sendShutdown(addr string) {
 	if err != nil {
 		return
 	}
+}
+
+func (s *ControlPlaneServer) Apply(l *raft.Log) any {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+	req := l.Data
+	op := req[0]
+
+	if op == '+' {
+		addr := string(req[1:])
+		if idx := slices.IndexFunc(s.Nodes, func(e Node) bool { return e.address == addr }); idx != -1 {
+			return &s.Nodes[idx]
+		}
+
+		id := s.Idx
+		s.Idx++
+		s.Nodes = append(s.Nodes, Node{id: id, address: addr})
+		if len(s.Nodes) > 1 {
+			go s.sendUpdate(s.Nodes[len(s.Nodes)-2].id)
+		}
+		return s.Nodes[len(s.Nodes)-1]
+	} else if op == '-' {
+		id_str := req[1:]
+		id, err := strconv.ParseInt(string(id_str), 10, 10)
+		if err != nil {
+			panic("invalid arg to '-' op")
+		}
+		idx := slices.IndexFunc(s.Nodes, func(e Node) bool { return e.id == id })
+		if idx == -1 {
+			return nil
+		}
+		if idx > 0 {
+			go s.sendUpdate(s.Nodes[idx-1].id)
+		}
+		if idx < len(s.Nodes)-1 {
+			go s.sendUpdate(s.Nodes[idx+1].id)
+		}
+
+		s.Nodes = slices.Delete(s.Nodes, idx, idx+1)
+	}
+	log.Panicf("unkown apply op %v", op)
+	return nil
+}
+
+func (s *ControlPlaneServer) Restore(r io.ReadCloser) error {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+	b, err := io.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	var snapshot Snapshot
+	err = json.Unmarshal(b, &snapshot)
+	if err != nil {
+		return err
+	}
+	s.Idx = snapshot.idx
+	copy(s.Nodes, snapshot.nodes)
+	return nil
+}
+
+type Snapshot struct {
+	idx   int64
+	nodes []Node
+}
+
+func (s *Snapshot) Persist(sink raft.SnapshotSink) error {
+	bytes, err := json.Marshal(s)
+	if err != nil {
+		return fmt.Errorf("sink.Write(): %v", err)
+	}
+	_, err = sink.Write(bytes)
+	if err != nil {
+		sink.Cancel()
+		return fmt.Errorf("sink.Write(): %v", err)
+	}
+	return sink.Close()
+}
+
+func (s *Snapshot) Release() {}
+
+func (s *ControlPlaneServer) Snapshot() (raft.FSMSnapshot, error) {
+	s.mtx.RLock()
+	defer s.mtx.RUnlock()
+	var new_nodes []Node
+	copy(new_nodes, s.Nodes)
+	return &Snapshot{idx: s.Idx, nodes: new_nodes}, nil
 }
